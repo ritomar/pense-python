258d12c661b88271ec550ba4cd0ac3012fbb5e1e
Ritomar Torquato <ritomar@gmail.com>
2020-10-17 21:55:48 -0300

    Concluído Exercício 1.1

---
+++ exercicio-01-01-03.py
@@ -0,0 +1,17 @@
+"""
+    Você pode usar um sinal de menos para fazer um número negativo como -2.
+    O que acontece se puser um sinal de mais antes de um número?
+    E se escrever assim: 2++2?
+
+    R. Aplica a regra dos sinais.
+        Adição/Subtração
+        → menos com menos: soma e conserva o sinal;
+        → mais com mais: soma e conserva o sinal;
+        → menos com mais: subtrai e conserva o sinal do "maior".
+
+        Multiplicação/Divisão
+        → menos com menos: dá mais;
+        → mais com mais: dá mais;
+        → mais com menos: dá menos.
+"""
+
---
+++ exercicio-01-01-04.py
@@ -0,0 +1,6 @@
+"""
+    Na notação matemática, zeros à esquerda são aceitáveis, como em 02.
+    O que acontece se você tentar usar isso no Python?
+    R. Um SintaxError. Zeros à esquerda não são aceitáveis.
+"""
+
