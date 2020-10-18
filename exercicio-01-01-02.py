f42da5339f257a55aefb0dc71121567cf3034070
Ritomar Torquato <ritomar@gmail.com>
2020-10-17 21:31:39 -0300

    Questão 1.1.2

---
+++ exercicio-01-01-02.py
@@ -0,0 +1,6 @@
+"""
+    Se estiver tentando imprimir uma string, o que acontece se omitir uma das aspas ou ambas?
+    R. Omitir uma das aspas: SyntaxError: EOL (End of line) while scanning string literal
+    Omitir ambas as aspas: NameError: name 'id' is not defined
+"""
+
