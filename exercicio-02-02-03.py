cc29b1efc08a609534d30dbfacc8a56e0870c4cb
Ritomar Torquato <ritomar@gmail.com>
2020-10-18 14:32:24 -0300

    Concluído Capítulo 2

--- exercicio-02-02-03.py
+++ exercicio-02-02-03.py
@@ -2,7 +2,7 @@
     Se eu sair da minha casa às 6:52 e correr 1 quilômetro a um certo passo (8min15s por quilômetro),
     então 3 quilômetros a um passo mais rápido (7min12s por quilômetro) e 1 quilômetro no mesmo passo
     usado em primeiro lugar, que horas chego em casa para o café da manhã?
-    R. 945.45
+    R. Hora de chegada: 07:30:06
 """
 
 
